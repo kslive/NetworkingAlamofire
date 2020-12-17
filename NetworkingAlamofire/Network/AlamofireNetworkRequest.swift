672fe12cca7aeb176467988cb63fb0c6397bce1d
Eugene <Kiselev>
2020-12-17 17:29:39 +0300

    оптимизация запроса с использованием Alamofire

--- NetworkingAlamofire/Network/AlamofireNetworkRequest.swift
+++ NetworkingAlamofire/Network/AlamofireNetworkRequest.swift
@@ -10,15 +10,17 @@ import Alamofire
 
 class AlamofireNetworkRequest {
     
-    static func sendRequest(url: String) {
+    static func sendRequest(url: String, completion: @escaping (_ courses: [Course]) -> ()) {
         
         guard let url = URL(string: url) else { return }
         
         AF.request(url, method: .get).validate().responseJSON { response in
-            
             switch response.result {
             case .success(let value):
-                print(value)
+                var courses = [Course]()
+                
+                courses = Course.getArray(from: value)!
+                completion(courses)
             case .failure(let error):
                 print(error)
             }
